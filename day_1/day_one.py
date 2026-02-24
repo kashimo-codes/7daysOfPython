7fbe9cce3b1c02fd4d19dba1b2f61d097fb2b9f9
RSA-Monarch <titytshabalala@gmail.com>
2026-02-24 23:07:48 +0200

    Add day 1 of course

---
+++ day_1/day_one.py
@@ -0,0 +1,20 @@
+# Day 1 of the 7 Days of Python
+# Learning about Operatoins & Expressions:
+
+# Variables
+a = 7 # Assign variable a the value 7
+b = x + 3 # Assign variable b the value pf plus 3
+c = b # Assign variable c the value of b
+
+# Variables names are case-sensitive and can contain
+# any letter, number, or underscore, though cannot begin with a number
+
+name = "My name is Tity Junior"
+sen = "This is also a string"
+
+y = 'My name is' + ' ' + 'Tity Junior'
+print(y) # outputs: My name is Tity Junior
+
+#Printing to the Console.
+
+print('Hello, World!')
